2c5c922f99b3ee78569b46d7ed26ff8abece9168
PranayChowdhury00 <pranaychowdhury00@gmail.com>
2025-06-30 13:01:15 +0600

    removing something

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json({ limit: '5mb' }));
 app.use(cors({
   origin: ['http://localhost:5173'
    ,'https://event-management-task-ph.vercel.app',
-  'https://event-management-backend-tawny.vercel.app'
   ],
   credentials: true 
 }));
